818bc9761bb73e908f45aa320bb2697dd9da5bc5
aparikh223 <34142658+aparikh223@users.noreply.github.com>
2019-08-23 12:12:38 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,8 +42,9 @@ const Container = styled.div`
 const HeroText = styled.h2`
   color: rgb(82, 113, 255);
   text-align: center;
-  text-shadow: 0px 2px 5px rgba(0,0,0,0.5);
-  font-size: 30px;
+  text-shadow: 0px 1px 2px rgba(0,0,0,0.5);
+  font-weight: 700
+  font-size: 40px;
   margin-bottom: 3rem;
 `
 
