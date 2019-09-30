ea19b860f1fac35c99a15f96cc36da37214bb135
aparikh223 <34142658+aparikh223@users.noreply.github.com>
2019-09-29 22:42:46 -0700

    Update gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,19 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+import React from "react"
+import { ServerStyleSheet, StyleSheetManager } from "styled-components"
 
-// You can delete this file if you're not using it
+const sheetByPathname = new Map()
+
+// eslint-disable-next-line react/prop-types,react/display-name
+exports.wrapRootElement = ({ element, pathname }) => {
+  const sheet = new ServerStyleSheet()
+  sheetByPathname.set(pathname, sheet)
+  return <StyleSheetManager sheet={sheet.instance}>{element}</StyleSheetManager>
+}
+
+exports.onRenderBody = ({ setHeadComponents, pathname }) => {
+  const sheet = sheetByPathname.get(pathname)
+  if (sheet) {
+    setHeadComponents([sheet.getStyleElement()])
+    sheetByPathname.delete(pathname)
+  }
+}
